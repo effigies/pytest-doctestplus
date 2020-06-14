f62d747e9e75fc4c6aff1225f8913ec0779a4930
Chris Markiewicz <markiewicz@stanford.edu>
2020-06-14 01:01:28 -0400

    ENH: Add a patched DocTestRunner to fix doctests

---
+++ pytest_doctestplus/doctest_runner.py
@@ -0,0 +1,151 @@
+import doctest
+import sys
+import traceback
+from pathlib import Path
+
+
+def fixup(test, example, got):
+    module_path = Path(test.filename)
+    module_text = module_path.read_text()
+    module_lines = module_text.splitlines()
+    want_lines = [' ' * example.indent + line
+                  for line in example.want.splitlines()]
+    lineno = test.lineno + example.lineno
+    for i in range(5):
+        if module_lines[lineno + i] == want_lines[0]:
+            lineno += i
+            break
+    else:
+        raise RuntimeError(f"Can't find {match} around line {lineno} of {test.filename}")
+    got_lines = [' ' * example.indent + line
+                 for line in got.splitlines()]
+    module_lines[lineno:lineno + len(want_lines)] = got_lines
+    module_path.write_text('\n'.join(module_lines + ['']))
+
+ 
+class DocTestRunner(doctest.DebugRunner):
+    def __run(self, test, compileflags, out):
+        """
+        Run the examples in `test`.  Write the outcome of each example
+        with one of the `DocTestRunner.report_*` methods, using the
+        writer function `out`.  `compileflags` is the set of compiler
+        flags that should be used to execute examples.  Return a tuple
+        `(f, t)`, where `t` is the number of examples tried, and `f`
+        is the number of examples that failed.  The examples are run
+        in the namespace `test.globs`.
+        """
+        # Keep track of the number of failures and tries.
+        failures = tries = 0
+
+        # Save the option flags (since option directives can be used
+        # to modify them).
+        original_optionflags = self.optionflags
+
+        SUCCESS, FAILURE, BOOM = range(3) # `outcome` state
+
+        check = self._checker.check_output
+
+        # Process each example.
+        for examplenum, example in enumerate(test.examples):
+
+            # If REPORT_ONLY_FIRST_FAILURE is set, then suppress
+            # reporting after the first failure.
+            quiet = (self.optionflags & doctest.REPORT_ONLY_FIRST_FAILURE and
+                     failures > 0)
+
+            # Merge in the example's options.
+            self.optionflags = original_optionflags
+            if example.options:
+                for (optionflag, val) in example.options.items():
+                    if val:
+                        self.optionflags |= optionflag
+                    else:
+                        self.optionflags &= ~optionflag
+
+            # If 'SKIP' is set, then skip this example.
+            if self.optionflags & doctest.SKIP:
+                continue
+
+            # Record that we started this example.
+            tries += 1
+            if not quiet:
+                self.report_start(out, test, example)
+
+            # Use a special filename for compile(), so we can retrieve
+            # the source code during interactive debugging (see
+            # __patched_linecache_getlines).
+            filename = '<doctest %s[%d]>' % (test.name, examplenum)
+
+            # Run the example in the given context (globs), and record
+            # any exception that gets raised.  (But don't intercept
+            # keyboard interrupts.)
+            try:
+                # Don't blink!  This is where the user's code gets run.
+                exec(compile(example.source, filename, "single",
+                             compileflags, True), test.globs)
+                self.debugger.set_continue() # ==== Example Finished ====
+                exception = None
+            except KeyboardInterrupt:
+                raise
+            except:
+                exception = sys.exc_info()
+                self.debugger.set_continue() # ==== Example Finished ====
+
+            got = self._fakeout.getvalue()  # the actual output
+            self._fakeout.truncate(0)
+            outcome = FAILURE   # guilty until proved innocent or insane
+
+            # If the example executed without raising any exceptions,
+            # verify its output.
+            if exception is None:
+                if not check(example.want, got, self.optionflags):
+                    fixup(test, example, got)
+                outcome = SUCCESS
+
+            # The example raised an exception:  check if it was expected.
+            else:
+                exc_msg = traceback.format_exception_only(*exception[:2])[-1]
+                if not quiet:
+                    got += doctest._exception_traceback(exception)
+
+                # If `example.exc_msg` is None, then we weren't expecting
+                # an exception.
+                if example.exc_msg is None:
+                    outcome = BOOM
+
+                # We expected an exception:  see whether it matches.
+                elif check(example.exc_msg, exc_msg, self.optionflags):
+                    outcome = SUCCESS
+
+                # Another chance if they didn't care about the detail.
+                elif self.optionflags & doctest.IGNORE_EXCEPTION_DETAIL:
+                    if check(doctest._strip_exception_details(example.exc_msg),
+                             doctest._strip_exception_details(exc_msg),
+                             self.optionflags):
+                        outcome = SUCCESS
+
+            # Report the outcome.
+            if outcome is SUCCESS:
+                if not quiet:
+                    self.report_success(out, test, example, got)
+            elif outcome is FAILURE:
+                if not quiet:
+                    self.report_failure(out, test, example, got)
+                failures += 1
+            elif outcome is BOOM:
+                if not quiet:
+                    self.report_unexpected_exception(out, test, example,
+                                                     exception)
+                failures += 1
+            else:
+                assert False, ("unknown outcome", outcome)
+
+            if failures and self.optionflags & FAIL_FAST:
+                break
+
+        # Restore the option flags (in case they were modified)
+        self.optionflags = original_optionflags
+
+        # Record and return the number of failures and tries.
+        self.__record_outcome(test, failures, tries)
+        return doctest.TestResults(failures, tries)
